0d9485ac03fdb37a2ed6a225ef2330353edc266d
Giritharan <70332966+giri14mct@users.noreply.github.com>
2021-09-09 00:34:38 +0530

    Update postDisplayer.js

--- src/components/PostDisplayer.js
+++ src/components/PostDisplayer.js
@@ -2,10 +2,29 @@ import React from "react"
 import './Post.css'
 
 const PostDisplayer = (props) => {
+
+    // console.log(props)
+
     return (
         <div className='final-post'>
-            <h1>{props.renderTxt}</h1>
-            <img src={props.renderUrl} alt='' />
+
+            {props.finalRender.map(data => {
+                return (
+                    <>
+                        <div className='post-name'>
+                            {data.renderUrl ? (<div className='post-head'>
+                                <h4>Code Mancers</h4>
+                                <p>{new Date().toLocaleTimeString()}</p>
+                            </div>) : null}
+                            <div className='post-msg'>
+                                <p>{data.renderTxt} </p>
+                                {data.renderUrl ? (<img src={data.renderUrl} alt="Post Will be display here" />) : null}
+                            </div>
+                        </div>
+
+                    </>
+                )
+            })}
         </div>
     )
 }
